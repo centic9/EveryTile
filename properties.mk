1cba67fdb296287656e729cb10778dc1e2cccd7f
Dominik Stadler <dominik.stadler@gmx.at>
2024-05-31 12:50:36 +0200

    Add support for running/building on the commandline

---
+++ properties.mk
@@ -0,0 +1,4 @@
+SDK_HOME = `cat /home/dstadler/.Garmin/ConnectIQ/current-sdk.cfg`
+DEPLOY = /tmp/com.garmin.connectiq/GARMIN/APPS/
+PRIVATE_KEY = /opt/garmin/developer_key
+JAVA_HOME = /usr/lib/jvm/java-17-openjdk-amd64/
